8300b7d4060838104dfbba6ef0a9d21a9d4167cb
Chris Cardle <cdcardle@gmail.com>
2017-10-27 22:34:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(array)
   if array.size == 0
     puts "The line is currently empty."
   else
-    deli.each do |name|
+    array.each do |name|
     i = array.index(name) + 1
     line.push("#{i}. #{name}")
     end
@@ -26,4 +26,3 @@ def now_serving(array)
     array.shift
   end
 end
-  
